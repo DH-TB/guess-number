b748fbdea3c971704c296e7460d6968b5a7f0f7f
DH-TB <929659475@qq.com>
2016-07-26 16:49:16 +0800

    Compare-Number.js

---
+++ src/Compare-Number.js
@@ -0,0 +1,19 @@
+class CompareNumber {
+    static guess(answer, input) {
+        const numbers = input.split('');
+        const answers = answer.split('');
+
+        const B = ()=> {
+            return numbers.map((number, index)=>
+                answers[index] != number && answers.some(n=>n == number) ? 1 : 0)
+                .reduce((a, b)=>a + b);
+        };
+        const A = ()=> {
+            return numbers.map((number, index)=>answers[index] === number ? 1 : 0)
+                .reduce((a, b)=>a + b);
+        };
+        return `${A()}A${B()}B`;
+    }
+}
+
+module.exports = CompareNumber;
