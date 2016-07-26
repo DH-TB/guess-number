98f778e667bdf655086643c8364a1aebdf68b38e
DH-TB <929659475@qq.com>
2016-07-26 16:52:23 +0800

    modify main-spec

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,9 +1,27 @@
-const CompareNumber = require('../src/app');
+const CompareNumber = require('../src/Compare-Number');
+const AnswerGenerator = require('../src/Answer-Generator');
 
-describe('guess number',()=>{
-        it('digit right and position right',()=>{
-            const inputs = '1234';
-            const answer = '4A0B';
-            expect(CompareNumber.guess(answer,inputs)).toEqual(answer);
-        });
+describe('guess number', ()=> {
+    const answers ='1234';
+
+    it('digit right and position right', ()=> {
+        const inputs = '1234';
+        const expectText = '4A0B';
+        expect(CompareNumber.guess(answers, inputs)).toEqual(expectText);
+    });
+    it('digit right and position error', ()=> {
+        const inputs = '4321';
+        const expectText = '0A4B';
+        expect(CompareNumber.guess(answers, inputs)).toEqual(expectText);
+    });
+    it('some digit right and position right', ()=> {
+        const inputs = '1243';
+        const expectText = '2A2B';
+        expect(CompareNumber.guess(answers, inputs)).toEqual(expectText);
+    });
+    it('digit error', ()=> {
+        const inputs = '5678';
+        const expectText = '0A0B';
+        expect(CompareNumber.guess(answers, inputs)).toEqual(expectText);
+    });
 });
